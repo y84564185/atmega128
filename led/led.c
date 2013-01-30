83e7dff1c281317cd2fcaf134fbb5148ba33cda7
yangyang <njitdkj082yy@sina.com>
2013-01-30 15:55:44 +0800

    <util/delay.h>:must define F_CPU MACRO first.

---
+++ led/led.c
@@ -0,0 +1,18 @@
+#include<avr/io.h>
+#define F_CPU 16000000UL
+#include<util/delay.h>
+int main()
+{
+    DDRE |= 0XFF;
+    PORTE|= 0XFF;
+    DDRF = 0XFF;
+    while(1)
+	{
+    	PORTF = 0XAA;
+		_delay_ms(1000);
+		PORTF = 0X55;
+		_delay_ms(1000);
+	}
+	return 0;
+}
+

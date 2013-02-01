196c6a9e313ea43c0320b20d30001d14d99b3cb4
yangyang <njitdkj082yy@sina.com>
2013-02-01 09:46:52 +0800

    uart:add function->PrintHex(uint8_t c)

--- uart/uart.c
+++ uart/uart.c
@@ -2,6 +2,8 @@
 #include"uart.h"
 #define F_CPU 16000000UL
 #include<util/delay.h>
+#include<inttypes.h>
+uint8_t HexTable[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 void uart_init(void)
 {
 	UCSR1B = 0X00;
@@ -29,26 +31,26 @@ char uart_getchar(void)
 	loop_until_bit_is_set(UCSR1A,RXC1);	//wait until data exists
 	return	UDR1;
 }
-
+void PrintHex(uint8_t c)
+{
+	uart_putchar('0');
+	uart_putchar('x');
+	uart_putchar(HexTable[c>>4]);
+	uart_putchar(HexTable[c&0x0f]);
+}
 int main()
 {
 	uart_init();
+	uint8_t w=0;
 	while(1)
 	{
-		uart_putchar('H');
-		_delay_ms(100);
-		uart_putchar('e');
-		_delay_ms(100);
-		uart_putchar('l');
-		_delay_ms(100);
-		uart_putchar('l');
-		_delay_ms(100);
-		uart_putchar('o');
-		_delay_ms(100);
-		uart_putchar('\n');
-		_delay_ms(100);
-		uart_putchar('\r');
-		_delay_ms(100);
+		for(w=0;w<256;w++)
+		{
+			PrintHex(w);
+			_delay_ms(100);
+			uart_putchar('\n');
+			uart_putchar('\r');
+		}
 	}
 	return 0;
 }
